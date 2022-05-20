c1158da8be1a6eb0bc0180ea740c3f9eeec2dacc
ManishKumarj7 <104965052+ManishKumarj7@users.noreply.github.com>
2022-05-20 14:28:29 +0530

    Add files via upload

---
+++ PojoDemo/src/com/manish/POJO/GetCourse.java
@@ -0,0 +1,12 @@
+package com.manish.POJO;
+
+public class GetCourse {
+
+	private String url;
+	private String services;
+	private String expertise;
+	private	String courses;
+	private String instructor;
+	private String linkedIn;
+
+}
